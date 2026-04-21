6c6ee3a4265d40a6e1aa4c48fd3bb0eb3ea38c46
Emanuel <eatiran39@gmail.com>
2026-04-21 19:25:52 +0300

    opdate domain

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // config.js
 export const API_URL =
   import.meta.env.MODE === "production"
-    ? "https://bneyhayeshivot.onrender.com"
+    ? "https://bneyhayeshivot1.onrender.com"
     : "http://localhost:5000";
