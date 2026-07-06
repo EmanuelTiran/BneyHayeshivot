753696de2c2c6ad810e58c117fb582c590a6e004
Emanuel <eatiran39@gmail.com>
2026-07-06 10:57:41 +0300

    node mailer 4

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -2,7 +2,7 @@ const { Resend } = require('resend');
 const User        = require('../models/User');
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
+const SITE_URL = process.env.SITE_URL || 'https://bneyhayeshivot-1.onrender.com/';
 /**
  * בונה את תוכן ה-HTML למייל
  * העיצוב מותאם 1:1 לקומפוננטת ContactAndPrayerTimes:
